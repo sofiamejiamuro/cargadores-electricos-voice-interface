e4e66d78390578dbbb2c6d12968699edbe0caa1a
sofiamejiamuro <sofiamejiamuro@gmail.com>
2020-08-28 00:19:28 -0500

    adding urls and index.js code

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Esta interfaz de voz se diseñó y desarrolló como propuesta de valor a la weba
  
 ## Conversation design
 
-[Aquí](https://github.com/sofiamejiamuro/quien-sabe-mas/blob/master/model.json) se puede ver el interaction model utilizado en el código.
+[Aquí](https://github.com/sofiamejiamuro/cargadores-electricos-voice-interface/blob/master/model.json) se puede ver el interaction model utilizado en el código.
 
 
 ## Intents && Utterances 
@@ -23,17 +23,12 @@ Para este MVP se definieron los siguientes *intents* con sus respectivas *uttera
 
 ## Código
 
-Una vez diseñada la conversación se desarrollo la skill con JavaScript en la alexa developer console. El codigo se puede ver [aquí](https://github.com/sofiamejiamuro/quien-sabe-mas/blob/master/index.js).
+Una vez diseñada la conversación se desarrollo la skill con JavaScript en la alexa developer console. El codigo se puede ver [aquí](https://github.com/sofiamejiamuro/cargadores-electricos-voice-interface/blob/master/index.js).
 
-<img src="./assets/images/alexa-developer-console.png"  width="800"/>
 
 ## Skill ¿Quién sabe más?
 
 A continuación se pueden escuchar dos grabaciones de la interación persona - Alexa en este juego.
 
-[quien-sabe-mas-1](https://drive.google.com/file/d/1G6whs9w3o9X2dfRzhWdMqKMtC2pCrWr7/view?usp=sharing)
-
-[quien-sabe-mas-2](https://drive.google.com/file/d/1mlnS5H1P6Uvk2m9o2MDq9NQu7c1jf4Nx/view?usp=sharing)
-
-[quien-sabe-mas-flujo-completo3](https://drive.google.com/file/d/1KZPo8ieuZu4QUOZTT92pIaRqigW4anCt/view?usp=sharing)
+[cargadores-electricos-1](https://drive.google.com/file/d/1H4QfCd9WAtEkBr5lJ0hiMP93qi_eucb8/view?usp=sharing)
 
---
+++ index.js
@@ -0,0 +1,187 @@
+// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+// session persistence, api calls, and more.
+const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hola,soy tu cargador eléctrico ¿en qué puedo ayudarte?';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hello World!';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+//Nearest Chargers 
+const NearestChargersIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'NearestChargersIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'El cargador más cercano es el de avenida universidad, te queda a tres kilometros, ¿quieres apartarlo?';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt('¿quieres saber cómo llegar?')
+            .getResponse();
+    }
+};
+
+//Book nearest charger 
+const BookNearestChargerIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'BookNearestChargerIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'apartando el cargador de avenida universidad, recuerda que tienes quince minutos antes de que se libere ¿te llevo?';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt('¿algo más?')
+            .getResponse();
+    }
+};
+
+//Get to the  place
+const TellTheRouteIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'TellTheRouteIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'para llegar al cargador de avenidad  universidad, avanza 200 metros, gira a la derecha, avanza 500 metros, llegamos';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt('¿te puedo ayudar en algo más?')
+            .getResponse();
+    }
+};
+
+
+//BateryLeft 
+const BateryLeftIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'BateryLeftIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Tienes veinte por ciento de bateria, suficiente para recorrer quince kilometros';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt('¿te puedo ayudar en algo más?')
+            .getResponse();
+    }
+};
+
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say hello to me! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.stack}`);
+        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        HelloWorldIntentHandler,
+        NearestChargersIntentHandler,
+         BookNearestChargerIntentHandler,
+        TellTheRouteIntentHandler,
+        BateryLeftIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    )
+    .addErrorHandlers(
+        ErrorHandler,
+    )
+    .lambda();
